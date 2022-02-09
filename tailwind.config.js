225a5884424c897c62eeed430ebd4c68503e2be7
nikhilsheoran <thenikhilsheoran@gmail.com>
2022-02-09 21:13:38 +0530

    last nextjs commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Galano Grotesque', 'Helvetica', 'Arial', 'sans-serif'],
+        'serif': ['Galindo', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
